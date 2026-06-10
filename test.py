9df118dbc63b0101e0953b1a09e7d9bd3b1ab28a
Ian Simpson <thefamilysimpson@gmail.com>
2026-06-10 11:31:58 +0000

    input validation - for printing

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+# Task - add some input validation to this
+lower = 4
+upper = 10
+
+
 age=int(input("How old are you?"))
+while age < lower or age > upper:
+    print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    age=int(input("How old are you?"))
 
 if age >= 17:
     print("you are old enough to drive")
